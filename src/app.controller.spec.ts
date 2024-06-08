e8dcd81835bb88b46d173b44ad0ac78f45fcfa9c
junior m. soares <jrmsrs2.0@gmail.com>
2024-06-08 02:11:06 -0300

    chore: Update app controller to return docs info

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return info', () => {
+      expect(appController.getRoot()).toStrictEqual({
+        message: 'swagger doc at /api',
+      });
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getRoot(): { message: string } {
+    return {
+      message: 'swagger doc at /api',
+    };
   }
 }
