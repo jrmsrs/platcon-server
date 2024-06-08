499637a4e520292a0b100d5cd4a14c27428e9af9
junior m. soares <jrmsrs2.0@gmail.com>
2024-06-08 01:33:26 -0300

    feat: Add Swagger documentation for API endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Platcon')
+    .setDescription(
+      `\
+A description`,
+    )
+    .setVersion('0.0.1')
+    //.addTag('Users', 'Rotas Usuários')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
