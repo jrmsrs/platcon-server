3b1788385a828664d5ea5a8cf6c02a7f994428d6
junior m. soares <jrmsrs2.0@gmail.com>
2024-06-11 00:18:17 -0300

    feat: Add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { DbModule } from './db/db.module'
 import { ConfigModule } from '@nestjs/config'
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DbModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [DbModule, ConfigModule.forRoot({ isGlobal: true }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
