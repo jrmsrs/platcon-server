f8be22b094f4b09408cf56147a9be1fccc07e04b
junior m. soares <jrmsrs2.0@gmail.com>
2024-06-27 00:13:00 -0300

    test(members.repository): Improve coverage

--- src/members/__tests__/members.repository.spec.ts
+++ src/members/__tests__/members.repository.spec.ts
@@ -3,11 +3,19 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { getRepositoryToken } from '@nestjs/typeorm'
 import { Repository, UpdateResult, DeleteResult, InsertResult } from 'typeorm'
 import { faker } from '@faker-js/faker'
+import { PostgresError as PgError } from 'pg-error-enum'
 
 import { MembersRepository } from '#members/members.repository'
 import { Member } from '#members/entities/member.entity'
 import { CreateMemberDto, UpdateMemberDto } from '#members/dto'
 import { memberMock, createMemberMock } from '#members/__mocks__'
+import {
+  NotFoundError,
+  StateConflictError,
+  UnaffectedError,
+  UnexpectedError,
+  UniqueViolationError,
+} from '#utils/errors'
 
 describe('MembersRepository', () => {
   let repository: MembersRepository
@@ -41,6 +49,28 @@ describe('MembersRepository', () => {
 
       expect(result).toEqual(member)
     })
+
+    it('should throw an error if member already exists', async () => {
+      const createMemberDto: CreateMemberDto = createMemberMock
+      jest.spyOn(memberRepository, 'insert').mockRejectedValue({ name: PgError.UNIQUE_VIOLATION })
+
+      try {
+        await repository.create(createMemberDto)
+      } catch (error) {
+        expect(error).toBeInstanceOf(UniqueViolationError)
+      }
+    })
+
+    it('should throw an error if an unexpected error occurs', async () => {
+      const createMemberDto: CreateMemberDto = createMemberMock
+      jest.spyOn(memberRepository, 'insert').mockRejectedValue(new Error())
+
+      try {
+        await repository.create(createMemberDto)
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnexpectedError)
+      }
+    })
   })
 
   describe('findAll', () => {
@@ -51,6 +81,16 @@ describe('MembersRepository', () => {
       const result = await repository.findAll()
 
       expect(result).toEqual(members)
+    })
+
+    it('should throw an error if an unexpected error occurs', async () => {
+      jest.spyOn(memberRepository, 'find').mockRejectedValue(new Error())
+
+      try {
+        await repository.findAll()
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnexpectedError)
+      }
     })
   })
 
@@ -63,6 +103,28 @@ describe('MembersRepository', () => {
       const result = await repository.findOne(id)
 
       expect(result).toEqual(member)
+    })
+
+    it('should throw an error if member is not found', async () => {
+      const id = faker.string.uuid()
+      jest.spyOn(memberRepository, 'findOne').mockResolvedValue(null)
+
+      try {
+        await repository.findOne(id)
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundError)
+      }
+    })
+
+    it('should throw an error if an unexpected error occurs', async () => {
+      const id = faker.string.uuid()
+      jest.spyOn(memberRepository, 'findOne').mockRejectedValue(new Error())
+
+      try {
+        await repository.findOne(id)
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnexpectedError)
+      }
     })
   })
 
@@ -77,6 +139,42 @@ describe('MembersRepository', () => {
 
       expect(result).toEqual(updateResult)
     })
+
+    it('should throw an error if member is not found', async () => {
+      const id = faker.string.uuid()
+      const updateMemberDto: UpdateMemberDto = { website: [faker.internet.url()] }
+      jest.spyOn(memberRepository, 'update').mockResolvedValue({ raw: [], affected: 0 } as UpdateResult)
+
+      try {
+        await repository.update(id, updateMemberDto)
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnaffectedError)
+      }
+    })
+
+    it('should throw an error if member already exists', async () => {
+      const id = faker.string.uuid()
+      const updateMemberDto: UpdateMemberDto = { website: [faker.internet.url()] }
+      jest.spyOn(memberRepository, 'update').mockRejectedValue({ name: PgError.UNIQUE_VIOLATION })
+
+      try {
+        await repository.update(id, updateMemberDto)
+      } catch (error) {
+        expect(error).toBeInstanceOf(UniqueViolationError)
+      }
+    })
+
+    it('should throw an error if an unexpected error occurs', async () => {
+      const id = faker.string.uuid()
+      const updateMemberDto: UpdateMemberDto = { website: [faker.internet.url()] }
+      jest.spyOn(memberRepository, 'update').mockRejectedValue(new Error())
+
+      try {
+        await repository.update(id, updateMemberDto)
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnexpectedError)
+      }
+    })
   })
 
   describe('removeMember', () => {
@@ -88,6 +186,39 @@ describe('MembersRepository', () => {
       const result = await repository.remove(id)
 
       expect(result).toEqual(deleteResult)
+    })
+
+    it('should throw an error if member is not found', async () => {
+      const id = faker.string.uuid()
+      jest.spyOn(memberRepository, 'delete').mockResolvedValue({ raw: [], affected: 0 } as DeleteResult)
+
+      try {
+        await repository.remove(id)
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnaffectedError)
+      }
+    })
+
+    it('should throw an error if trying to delete a member with restrict constraint', async () => {
+      const id = faker.string.uuid()
+      jest.spyOn(memberRepository, 'delete').mockRejectedValue({ name: PgError.FOREIGN_KEY_VIOLATION })
+
+      try {
+        await repository.remove(id)
+      } catch (error) {
+        expect(error).toBeInstanceOf(StateConflictError)
+      }
+    })
+
+    it('should throw an error if an unexpected error occurs', async () => {
+      const id = faker.string.uuid()
+      jest.spyOn(memberRepository, 'delete').mockRejectedValue(new Error())
+
+      try {
+        await repository.remove(id)
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnexpectedError)
+      }
     })
   })
 })
